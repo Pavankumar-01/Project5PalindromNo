c641440906e9c1cc94983fc452570c2e7493fbdb
Pavankumar-01 <pavan.kreeekiit@gmail.com>
2020-07-16 21:46:28 +0530

    initial commit

---
+++ Project5PalindromNo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project5PalindromNo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, rem, sum = 0, temp;
+            Console.Write("Enter the Number to Check : ");
+            n = Convert.ToInt16(Console.ReadLine());
+            temp = n;
+            while (n > 0)
+            {
+                rem = n % 10;
+                sum = (sum * 10) + rem;
+                n = n / 10;
+            }
+            if (temp == sum)
+                Console.Write("Entered Number is Palindrome\n");
+            else
+                Console.Write("Entered Number is not Palindrome\n");
+        }
+    }
+}
